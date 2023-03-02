ee3b053b2ec15564ef1c763fbe39478ecc227a67
princeemekajoel <emekap547@gmail.com>
2023-03-02 04:22:59 -0800

    ./6-cap

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -18,24 +18,4 @@ i = 0;
 	}
 	return (s);
 }
-/*Bwave ICT */#include "main.h"
-/**
- * string_toupper - change lowercase to uppercase
- * @s:string
- * Return:char
- */
-char *string_toupper(char *s)
-{
-
-	int i;
-
-i = 0;
-	while (*(s + i))
-	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
-		i++;
-	}
-	return (s);
-}
 /*Prince Joel */
