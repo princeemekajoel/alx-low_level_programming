248f5ccbd5dbbfacf1c78ca6e1eb208682f2161e
princeemekajoel <emekap547@gmail.com>
2024-01-12 18:53:54 -0800

    My completion of recursions

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _pow_recursion - Returns the value of x raised to the power of y
+* @x: the valuie to multiply
+* @y: the times to pultiply the value
+* Return: the value multiplied y times
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
